63133c326b29bce0bee7a6893c34fecda4cee491
mkorolev <mkorolev@mmtr.ru>
2023-03-16 13:11:28 +0300

    add title icon

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
+            favicon: "./src/images/eng.png"
         })
     ]
 }
